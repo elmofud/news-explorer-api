3484f86b81b42e0b5aa0ff13d781bd1f85914c87
Tracey Lee <traceylg43@gmail.com>
2026-05-28 13:04:35 -0400

    Add working Express server with root route and MongoDB connection

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-/
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const { PORT = 3001 } = process.env;
+mongoose.connect("mongodb://127.0.0.1:27017/news_explorer_db");
+
+app.get("/", (req, res) => {
+  res.send("News Explorer API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
