52abcfc92d9a30283d4a2102bbd4fac2d65ab44a
Tracey Lee <traceylg43@gmail.com>
2026-05-31 22:25:13 -0400

    Add POST /Signup endpoint with routes and controller

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
+const routes = require("./routes/index");
 
+const app = express();
 const { PORT = 3001 } = process.env;
+
 mongoose.connect("mongodb://127.0.0.1:27017/news_explorer_db");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("News Explorer API is running");
 });
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
