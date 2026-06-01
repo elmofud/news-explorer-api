2fcd2fb4c061db992639303b1bc30cd8d55d22b6
Tracey Lee <traceylg43@gmail.com>
2026-06-01 13:03:29 -0400

    Add POST /signin with JWT and findUserByCredentials

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 
 const createUser = (req, res) => {
   const { name, email, password } = req.body;
@@ -16,4 +18,21 @@ const createUser = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports = { createUser };
+const login = (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).send({ message: "Email and password are required" });
+  }
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      return res.send({ token });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { createUser, login };
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const { createUser } = require("../controllers/users");
+const { createUser, login } = require("../controllers/users");
 
 router.post("/signup", createUser);
+router.post("/signin", login);
+
 module.exports = router;
