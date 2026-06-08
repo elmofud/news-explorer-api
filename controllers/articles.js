012a39c82c468d1628d64d8ae020a42cb9f82403
Tracey Lee <traceylg43@gmail.com>
2026-06-07 23:04:06 -0400

    Add POST /articles route with auth and validation

--- controllers/articles.js
+++ controllers/articles.js
@@ -6,4 +6,18 @@ const getArticles = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports = { getArticles };
+const createArticle = (req, res) => {
+  const { keyword, title, text, date, source, link, image } = req.body;
+  const owner = req.user._id;
+  Article.create({ keyword, title, text, date, source, link, image, owner })
+    .then((article) => res.status(201).send({ data: article }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).send({ message: "Invalid Data" });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+};
+
+module.exports = { getArticles, createArticle };
--- routes/articles.js
+++ routes/articles.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
-const { getArticles } = require("../controllers/articles");
+const { getArticles, createArticle } = require("../controllers/articles");
 
 router.get("/", auth, getArticles);
+router.post("/", auth, createArticle);
 
 module.exports = router;
