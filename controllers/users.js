7f85293888201fab71aa0c4ac53f70fdbdc542ce
Tracey Lee <traceylg43@gmail.com>
2026-06-14 23:59:12 -0400

    fix centralize error handling across all controllers with the custom error classes from text with postman

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,13 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const ConflictError = require("../errors/ConflictError");
+const BadRequestError = require("../errors/BadRequestError");
+const NotFoundError = require("../errors/NotFoundError");
+const UnauthorizedError = require("../errors/UnauthorizedError");
 const { JWT_SECRET } = process.env;
 
-const createUser = (req, res) => {
+const createUser = (req, res, next) => {
   const { name, email, password } = req.body;
   return bcrypt
     .hash(password, 10)
@@ -15,20 +19,36 @@ const createUser = (req, res) => {
       delete userObject.password;
       return res.status(201).send({ data: userObject });
     })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        next(new BadRequestError("Invalid data"));
+      } else if (err.code === 11000) {
+        next(new ConflictError("A user with this email already exists"));
+      } else {
+        next(err);
+      }
+    });
 };
 
-const getCurrentUser = (req, res) => {
+const getCurrentUser = (req, res, next) => {
   User.findById(req.user._id)
-    .orFail()
+    .orFail(() => {
+      throw new NotFoundError("User not found");
+    })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === "CastError") {
+        next(new BadRequestError("Invalid Id"));
+      } else {
+        next(err);
+      }
+    });
 };
 
-const login = (req, res) => {
+const login = (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).send({ message: "Email and password are required" });
+    return next(new BadRequestError("email and password are required"));
   }
   return User.findUserByCredentials(email, password)
     .then((user) => {
@@ -38,7 +58,12 @@ const login = (req, res) => {
       return res.send({ token });
     })
     .catch((err) => {
-      res.status(500).send({ message: err.message });
+      console.log("LOGIN ERROR:", err.name, "|", err.message);
+      if (err.message === "Incorrect email or password") {
+        next(new UnauthorizedError("Incorrect email or password"));
+      } else {
+        next(err);
+      }
     });
 };
 
